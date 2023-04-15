60c88494705f22f51e36d06e35a51eff2a6e8df2
Abhishek Ranjan <aviranjan444@gmail.com>
2023-04-15 19:09:40 +0530

    key changed

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 from langchain.chat_models import ChatOpenAI
 import gradio as gr
+import config
 import sys
 import os
 
-os.environ["OPENAI_API_KEY"]='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'
+os.environ["OPENAI_API_KEY"]=config.key
 
 def construct_index(directory_path):
     max_input_size = 4096
